33d6abc35313ff9c171211a9d74f65321894244d
Mike Riess <mike@riess.no>
2025-02-21 18:01:00 +0100

    model update

--- run_vllm.sh
+++ run_vllm.sh
@@ -1,5 +1,5 @@
 python -m vllm.entrypoints.openai.api_server \
-    --model meta-llama/Meta-Llama-3.3-70B-Instruct \
+    --model meta-llama/llama-3.1-405b-instruct-fp8 \
     --gpu-memory-utilization 0.9 \
     --tensor-parallel-size 8 \
     --host 0.0.0.0 \
