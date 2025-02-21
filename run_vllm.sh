56bea2a99828cfbda39596cb526f3dcebae414a9
Mike Riess <mike@riess.no>
2025-02-21 18:04:17 +0100

    modelname fix

--- run_vllm.sh
+++ run_vllm.sh
@@ -1,5 +1,5 @@
 python -m vllm.entrypoints.openai.api_server \
-    --model meta-llama/llama-3.1-405b-instruct-fp8 \
+    --model meta-llama/Llama-3.1-405B-Instruct-FP8 \
     --gpu-memory-utilization 0.9 \
     --tensor-parallel-size 8 \
     --host 0.0.0.0 \
