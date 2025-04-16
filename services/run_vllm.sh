e169e82e1c891427d1fec4810dc613340b4f9d82
Mike Riess <mikeriess@me.com>
2025-04-16 11:54:30 +0200

    fix json error

--- services/run_vllm.sh
+++ services/run_vllm.sh
@@ -1,6 +1,6 @@
 python -m vllm.entrypoints.openai.api_server \
-    --model meta-llama/Llama-3.1-405B-Instruct-FP8 \
-    --gpu-memory-utilization 1.0 \
-    --tensor-parallel-size 8 \
+    --model google/gemma-2-27b-it \
+    --gpu-memory-utilization 0.95 \
+    --tensor-parallel-size 4 \
     --host 0.0.0.0 \
     --port 8000
